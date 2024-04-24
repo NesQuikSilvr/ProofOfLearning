cd7f20bbaf3f4594f9415c497c0cc87811d7ed42
NesQuikSilvr <jkn002@ucsd.edu>
2024-04-23 21:06:03 -0700

    Team 3 format and extra

---
+++ Team 3/extra.c
@@ -0,0 +1,37 @@
+/*
+    EXTRA INSTRUCTIONS for Sayed, Brandon, Phi-Long, and Jonathan:
+
+    Team 3's robot arm used a motor-powered joint instead of a servo joint.
+    
+    The robot arm could be lifted by running the motor and using msleep()
+    to determine how long to move the pulley for.
+
+    However, it is more accurate to use the motor position counter to
+    wind specific lengths of the pulley string.
+
+    A function for using the pulley in this manner is partially written.
+
+    Complete the function definition for moveArm()
+*/
+
+
+const int pulley_port = 0;
+
+/* Use POSITIVE ticks value for UP, use NEGATIVE ticks value for DOWN */
+void moveArm(int ticks) {
+    //Clear the current motor position
+    cmpc(/*YOUR CODE HERE*/);
+
+    //UP
+    while(gmpc(/*YOURCODEHERE*/) < ticks) {
+        motor(/*YOURCODEHERE*/);
+    }
+
+    //UP
+    while(/*YOURCODEHERE*/) {
+        motor(/*YOURCODEHERE*/);
+    }
+
+    //Stop the motor
+    /*YOURCODEHERE*/
+}
--- Team 3/main.c
+++ Team 3/main.c
@@ -24,6 +24,9 @@
     Write the code for each mission function in their respective "missionX.c" file.
 
     Call each mission function in main() below.
+
+
+    EXTRA INSTRUCTIONS FOR Sayed, Brandon, Phi-Long, and Jonathan: also work on "extra.c"
 */
 
 int main() {
