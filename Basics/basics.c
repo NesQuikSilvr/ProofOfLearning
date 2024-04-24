af430f6b9c2b92697246f91624888de488cbe55b
NesQuikSilvr <jkn002@ucsd.edu>
2024-04-23 20:22:53 -0700

    Update basics.c

--- Basics/basics.c
+++ Basics/basics.c
@@ -3,7 +3,11 @@
 
 /*
     
-    
+    Pick up a can using the servo arm and claw, then return it to the starting area
+
+    Make sure your code is well-organized with spacing and comments
+
+    Use variables for motor and servo ports
 
 */
 
